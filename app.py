e1991bb8f4cf0ce7181d3839be7fa9954600fe8b
Alex Sparrow <alexsparrow5973@gmail.com>
2022-06-13 23:29:42 +0300

    fix model.py and accordion html components

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Learn Pyton"
+    title = "Learn Python Web"
+    return render_template('index.html', page_title=title)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
