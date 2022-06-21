512a14dff33b57f93152bc0140138016019649b2
Alex Sparrow <alexsparrow5973@gmail.com>
2022-06-21 22:22:50 +0300

    database update

--- app/__init__.py
+++ app/__init__.py
@@ -17,10 +17,37 @@ def create_app():
     admin.add_view(ModelView(Content, db.session))
     admin.add_view(ModelView(User, db.session))
 
+
     @app.route("/")
     def index():
         title = "Learn Python Web"
-        return render_template('index.html', page_title=title)
+        tracks =  Track.query.all()
+        sprints = Sprint.query.filter_by(track_id=1).all()
+
+        context = {
+            "tracks": tracks,
+            "sprints": sprints,
+        }
+
+        return render_template('index.html', page_title=title, **context)
+
+
+    # @app.route("/track/<int:pk>")
+    # def track(pk):
+    #     tracks = Track.query.all()
+    #     sprints=current_sprint
+    #     current_track = Track.query.filter_by(id=pk).first()
+    #     current_sprint = Sprint.query.filter_by(track_id=current_track).first()
+    #     current_lesson = Lesson.query.filter_by(sprint_id=current_sprint).first()
+
+    #     context = {
+    #         "tracks": tracks,
+    #         "current_lesson": current_lesson,
+    #         "current_sprint": current_sprint,
+    #         "current_track": current_track,
+    #     }
+
+    #     return render_template('track.html', **context)
 
     return app
 
