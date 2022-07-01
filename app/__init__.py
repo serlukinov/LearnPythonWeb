94c4d03540d4731db0da72c9c2a146e2f90916ce
AlexSparrow5973 <alexsparrow5973@gmail.com>
2022-07-01 17:29:17 +0300

    upgrade views

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask, flash, render_template, url_for
 
-# from flask_admin import Admin
-# from flask_admin.contrib.sqla import ModelView
-# from app.lessons.models import Content, Lesson, Sprint, Track
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager, current_user, login_required
 from flask_migrate import Migrate
 
 from app.db import db
-from app.admin.views import blueprint as admin_blueprint
+from app.lessons.models import Content, Lesson, Sprint, Track, progress
+from app.users.models import User
+
+from app.admins.views import blueprint as admin_blueprint
 from app.lessons.views import blueprint as lessons_blueprint
-from app.user.models import User
-from app.user.views import blueprint as user_blueprint
+from app.users.views import blueprint as user_blueprint
 
 
 def create_app():
@@ -21,18 +22,18 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = "user.login"
+    login_manager.login_view = "users.login"
 
     app.register_blueprint(admin_blueprint)
     app.register_blueprint(lessons_blueprint)
     app.register_blueprint(user_blueprint)
 
-    # admin = Admin(app, name="Admin", template_mode="bootstrap4")
-    # admin.add_view(ModelView(Track, db.session))
-    # admin.add_view(ModelView(Sprint, db.session))
-    # admin.add_view(ModelView(Lesson, db.session))
-    # admin.add_view(ModelView(Content, db.session))
-    # admin.add_view(ModelView(User, db.session))
+    admin = Admin(app, name="Admin", template_mode="bootstrap4")
+    admin.add_view(ModelView(Track, db.session))
+    admin.add_view(ModelView(Sprint, db.session))
+    admin.add_view(ModelView(Lesson, db.session))
+    admin.add_view(ModelView(Content, db.session))
+    admin.add_view(ModelView(User, db.session))
 
     @login_manager.user_loader
     def load_user(user_id):
